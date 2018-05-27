64be9db771bd4eb072ba135d6f888b6d5b11ce9c
Adrian Loer <adrian.loer@gmail.com>
2018-05-27 14:46:49 +0200

    without  this change we cant use vue dev tools somehow

--- fabric_vue/src/main.js
+++ fabric_vue/src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
+// import Vue from 'vue'
+import Vue from 'vue/dist/vue.js';
 import App from './App.vue'
 import store from './store'
 
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+//   store,
+// }).$mount('#app')
 new Vue({
-  render: h => h(App),
-  store,
-}).$mount('#app')
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  store
+})
